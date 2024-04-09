67894b3e9a0dba442076d73d8b63711ab16fdadc
Sushma Hegde <hegde.sus@northeastern.edu>
2024-04-08 18:41:35 -0700

    Changing to cloudfare ipfs url

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,6 @@
 export const GetIpfsUrlFromPinata = (pinataUrl) => {
     var IPFSUrl = pinataUrl.split("/");
     const lastIndex = IPFSUrl.length;
-    IPFSUrl = "https://ipfs.io/ipfs/"+IPFSUrl[lastIndex-1];
+    IPFSUrl = "https://cloudflare-ipfs.com/ipfs/"+IPFSUrl[lastIndex-1];
     return IPFSUrl;
 };
